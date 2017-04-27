dcf9d39c36029a257f33aa5d481f94c174bf7499
ClaudeSeo <ehdaudtj@gmail.com>
2017-04-28 01:11:56 +0900

    add configuration file

---
+++ functions/push/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+config = {
+    'github': {
+        'branch': {
+            'faustring/kec-app-server': ['@Claude']
+        },
+        'id': '',
+        'password': ''
+    },
+    'telegram': {
+        'token': '',
+        'chat_id': ''
+    }
+}
